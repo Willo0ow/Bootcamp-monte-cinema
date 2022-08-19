c545d682ba17a1f57857cfe9f7c81b64d3e36ec5
Dorota Hinc <dhinc39@gmail.com>
2022-08-19 09:46:49 +0200

    add contact us section

--- src/components/HomePage/ContactUs.vue
+++ src/components/HomePage/ContactUs.vue
@@ -1,9 +1,36 @@
 <template>
-  <div>{{ testData }}</div>
+  <div class="center-self" style="width: fit-content; margin-top: 188px">
+    <SectionTitle size="64" class="mb-48" style="text-align: center;">
+      Contact Us
+    </SectionTitle>
+    <div class="contact-card">
+      <div class="mb-40" style="display: flex">
+        <LocationIcon style="margin-right: 16px" />
+        <div style="display: inline-block">
+          Monterail Sp. z o.o.<br />
+          ul. Oławska 27-29<br />
+          50-123 Wrocław<br />
+          Poland
+        </div>
+      </div>
+      <div style="display: flex" class="mb-40">
+        <EmailIcon style="margin-right: 16px" />
+        <div>hello@monterail.com</div>
+      </div>
+      <div style="display: flex">
+        <PhoneIcon style="margin-right: 16px" />
+        <div>+48 533 600 136</div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+import SectionTitle from "@components/common/SectionTitle.vue";
+import LocationIcon from "@assets/images/icons/locationIcon.svg?component";
+import PhoneIcon from "@assets/images/icons/phoneIcon.svg?component";
+import EmailIcon from "@assets/images/icons/emailIcon.svg?component";
 export default {
-  components: {},
+  components: { SectionTitle, LocationIcon, PhoneIcon, EmailIcon },
   setup() {
     const testData = "Contact Us";
     return {
@@ -12,3 +39,15 @@ export default {
   },
 };
 </script>
+<style lang="scss" scoped>
+.contact-card {
+  width: 600px;
+  margin-bottom: 124px;
+  background: $gray-athens;
+  padding: 64px;
+  border-radius: 24px;
+  @include font-roboto-mono(18px, 400, 32px, $gray-tuna);
+  letter-spacing: 0.015em;
+  box-sizing: border-box;
+}
+</style>
